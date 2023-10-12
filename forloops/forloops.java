fb82b6bebe9930c565b7177475ca58bc71671fb2
Emery343 <143217107+Emery343@users.noreply.github.com>
2023-10-11 21:52:27 -0700

    Add files via upload

---
+++ forloops/forloops.java
@@ -0,0 +1,15 @@
+public class forloops {
+
+    public static void main(String[] args) { 
+
+        for (int i = 0; i < 15; i++) {
+            System.out.println ("i is less than 15");
+        }
+        for (int j = 10; j >= 0; j = j - 2) {
+            System.out.println ("j is between 0 and 10");
+        }
+        for (int k = 10; k >= 0; k -= 3) {
+            System.out.println ("k is between 0 and 10");
+        }
+    }
+}
